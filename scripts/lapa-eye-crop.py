d157913093282845e34c673e4e5879925600795f
jhanmtl <jing.han@mail.mcgill.ca>
2021-12-20 08:53:19 -0500

    got unityeyes data, landmarks eda

--- scripts/lapa-eye-crop.py
+++ scripts/lapa-eye-crop.py
@@ -1,4 +1,3 @@
-from src.data_tools import eye_crop
 import os
 import numpy as np
 from PIL import Image
@@ -41,6 +40,30 @@ def load_paths(workdir):
         assert img_id==label_id
     
     return imgpaths,labelpaths
+
+def eye_crop(img,mask,padding_ratio=0.75,min_maskratio=0.001):
+    
+    mask_ratio=np.sum(mask)/np.sum(np.ones_like(mask))    
+    if mask_ratio<min_maskratio:
+        raise Exception("minimum mask ratio not met")
+    
+    maskcoords=np.argwhere(mask)
+    [y1,x1]=np.min(maskcoords,0)
+    [y2,x2]=np.max(maskcoords,0)
+
+    h,w,_=img.shape
+
+    padding=int((x2-x1)*padding_ratio)
+    
+    y1=max(y1-padding,0)
+    x1=max(x1-padding,0)
+    
+    y2=min(y2+padding,h)
+    x2=min(x2+padding,w)
+    
+    cropped=img[y1:y2,x1:x2,:]
+    
+    return cropped
 
 def crop_serialize(img_path,cropkind,img,mask,padding,threshold,hf):
     try:
